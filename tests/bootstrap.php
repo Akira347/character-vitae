00003a3f2fb19278eafdce5e8588cf3c6df7dd7d
Akira <e.fonquernie@gmail.com>
2025-07-07 14:01:08 +0200

    chore: normalisation des fins de ligne en LF

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Component\Dotenv\Dotenv;
-
-require \dirname(__DIR__).'/vendor/autoload.php';
-
-// On sait que bootEnv existe à partir de Symfony 5+, on peut forcer l’appel
-(new Dotenv())->bootEnv(\dirname(__DIR__).'/.env');
