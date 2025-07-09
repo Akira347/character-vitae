da620bcd630b27134ccd40f5b3429b000dc35202
Akira <e.fonquernie@gmail.com>
2025-07-09 16:11:26 +0200

    Structure de base du layout : Header, Footer, Layout et HomePage

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/HomePage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+);
